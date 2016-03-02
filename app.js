955631028c83ffda99d329b1e09137db864500a1
dipu-bd <dipu.sudipta@gmail.com>
2016-03-03 01:31:05 +0600

    Database template code added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+ 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -55,6 +55,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var database = require('../database');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    checkRequest(req, function (logon) {
+        if (logon)
+            res.render('main-page', resource);
+        else
+            res.render('index', resource);
+    });
 });
+
+function checkRequest(req, callback) {
+    var sessionID = req.cookies['session'];
+    database.getUser(sessionID, function (err, result) {
+        callback(result);
+    });
+}
+
+var resource = {
+    title: 'Vaccine for Children'
+};
 
 module.exports = router;
