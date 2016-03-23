dea4acbb2f4e7186d05c115b06c03a86e9d009f2
Sudipto Chandra <dipu.sudipta@gmail.com>
2016-03-24 00:33:01 +0600

    Revert "database remodeled"
    This reverts commit 1505d35d707fcc8dfe30f3a4d78243c1f2be5072.

--- _database/vaccinedb.sql
+++ _database/vaccinedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2016 at 06:31 PM
+-- Generation Time: Mar 23, 2016 at 04:11 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -23,58 +23,46 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dose`
+-- Table structure for table `taken`
 --
 
-CREATE TABLE IF NOT EXISTS `dose` (
+CREATE TABLE IF NOT EXISTS `taken` (
   `id` int(11) NOT NULL,
-  `dab` bigint(20) NOT NULL DEFAULT '0',
-  `period` bigint(20) NOT NULL DEFAULT '86400000',
-  `vaccine` int(11) NOT NULL,
-  `name` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=ucs2;
-
---
--- Dumping data for table `dose`
---
-
-INSERT INTO `dose` (`id`, `dab`, `period`, `vaccine`, `name`) VALUES
-(1, 0, 86400000, 1, 'Dose 1'),
-(2, 28, 86400000, 1, 'Dose 2'),
-(3, 112, 86400000, 1, 'Dose 3'),
-(4, 42, 86400000, 2, 'Dose 1'),
-(5, 70, 86400000, 2, 'Dose 2'),
-(6, 98, 86400000, 2, 'Dose 3');
+  `child` int(11) NOT NULL,
+  `dose` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `dose`
+-- Indexes for table `taken`
 --
-ALTER TABLE `dose`
+ALTER TABLE `taken`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idx_dose__vaccine` (`vaccine`);
+  ADD KEY `child` (`child`),
+  ADD KEY `dose` (`dose`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `dose`
+-- AUTO_INCREMENT for table `taken`
 --
-ALTER TABLE `dose`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+ALTER TABLE `taken`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `dose`
+-- Constraints for table `taken`
 --
-ALTER TABLE `dose`
-  ADD CONSTRAINT `fk_dose__vaccine` FOREIGN KEY (`vaccine`) REFERENCES `vaccine` (`id`) ON DELETE CASCADE;
+ALTER TABLE `taken`
+  ADD CONSTRAINT `fk_taken_child` FOREIGN KEY (`child`) REFERENCES `child` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_taken_dose` FOREIGN KEY (`dose`) REFERENCES `dose` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
